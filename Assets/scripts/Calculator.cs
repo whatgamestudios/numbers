c9b5c18992192ca7315adbd44986c518b625986d
Peter Robinson <drinkcoffee@eml.c>
2024-12-13 13:34:24 +1000

    reworked GUI to have calculated and points

--- Assets/scripts/Calculator.cs
+++ Assets/scripts/Calculator.cs
@@ -12,16 +12,10 @@ public class Calculator : MonoBehaviour {
 
     private string currentInput = "";
 
-    private string resultText = "";
-
-    private double result = 0.0;
-
-    private double targetValue = 765;
+    private double targetValue = 0;
 
     public void Start() {
-        byte[] seed = SeedGen.GenerateSeed(0, 0);
-        uint val = SeedGen.GetNextValue(seed, 0, 1000);
-        targetValue = val;
+        startANewDay();
     }
 
 
@@ -34,33 +28,60 @@ public class Calculator : MonoBehaviour {
                 currentInput = currentInput.Substring(0, currentInput.Length - 1);
             }
         }
+        else if (buttonText == "=") {
+            CalculateResult();
+            // TODO indicate end of round
+            return;
+        }
         else {
             currentInput += buttonText;
         }
-        CalculateResult();
-
+        input.text = currentInput;
     }
 
     public void CalculateResult() {
+        // If the last character will make the equation invalid, remove it.
+        if (currentInput.EndsWith("+") || currentInput.EndsWith("-") || 
+            currentInput.EndsWith("*") || currentInput.EndsWith("/")) {
+            currentInput = currentInput.Substring(0, currentInput.Length - 1);
+        }
+        // If the brackets don't match, add extra brackets.
+        int left = 0;
+        int right = 0;
+        foreach (char c in currentInput) {
+            if (c == '(') {
+                left++;
+            }
+            else if (c == ')') {
+                right++;
+            }
+        }
+        for (uint i = 0; i < (left - right); i++) {
+            currentInput += ')';
+        }
+
+        string resultText;
         try {
-            result = System.Convert.ToDouble(new System.Data.DataTable().Compute(currentInput, ""));
+            double result = System.Convert.ToDouble(new System.Data.DataTable().Compute(currentInput, ""));
             resultText =  result.ToString();
-            UpdateDisplay();
         }
         catch (System.Exception ) {
             resultText = "Error";
-            UpdateDisplay();
         }
-    }
-
-    public void UpdateDisplay() {
-        input.text = currentInput;
-        target.text = targetValue.ToString();
         calculated.text = resultText;
-    }
+        input.text = currentInput;
+    }   
 
     public void Update() {
         timeToNext.text = Timeline.TimeToNextDayStr();
+    }
+
+    private void startANewDay() {
+        byte[] seed = SeedGen.GenerateSeed(0, 0);
+        uint val = SeedGen.GetNextValue(seed, 0, 1000);
+        targetValue = val;
+        target.text = val.ToString();
+        calculated.text = "";
     }
 
 }
