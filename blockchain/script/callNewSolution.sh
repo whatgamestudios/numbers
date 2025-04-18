05f3fdbf2306d07f6b34a3872cc40c578aef4358
Peter Robinson <drinkcoffee@eml.c>
2025-04-18 13:35:38 +1000

    Deployment script for gen1 NFTs

--- blockchain/script/callNewSolution.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-forge script --rpc-url https://rpc.immutable.com \
-    --priority-gas-price 10000000000 \
-    --with-gas-price     10000000100 \
-    -vvv \
-    --private-key $PKEY \
-    script/NewSolution.s.sol:NewSolution
-
