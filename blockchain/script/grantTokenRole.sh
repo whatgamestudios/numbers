8f1231fd188592dbe1d6e4a9ecda6c5de3228c57
Peter Robinson <drinkcoffee@users.noreply.github.com>
2025-05-10 16:25:22 +1000

    Add script to populate claim contract and update to V2 and then V3.

---
+++ blockchain/script/grantTokenRole.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# useMainNet: 1 for mainnet, 0 for testnet
+useMainNet=1
+# useLedger: 1 for ledger, 0 for private key
+useLedger=1
+
+LEDGER_HD_PATH="m/44'/60'/0'/0/1" 
+
+FUNCTION_TO_EXECUTE='grantTokenRole()'
+
+# Set-up variables and execute forge
+source $(dirname "$0")/common.sh
---
+++ blockchain/src/FourteenNumbersClaimV2.sol
@@ -0,0 +1,98 @@
+// Copyright (c) Whatgame Studios 2024 - 2025
+// SPDX-License-Identifier: PROPRIETORY
+pragma solidity ^0.8.28;
+
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {AccessControlEnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlEnumerableUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/erc1155/IERC1155.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/token/erc1155/IERC1155Receiver.sol";
+
+import {FourteenNumbersSolutionsV2} from "./FourteenNumbersSolutionsV2.sol";
+import {PassportCheck} from "./PassportCheck.sol";
+import {FourteenNumbersClaim} from "./FourteenNumbersClaim.sol";
+
+
+/**
+ * Allow players to submit claim requests.
+ *
+ * Each ERC1155 NFT added to this contract has a percentage likely to be returned and the
+ * number available for claim. NFTs can be given the percentage likelihood of claim of 0%.
+ * This indicates that the token is a fallback option if other claims aren't going to be 
+ * returned.
+ *
+ * The algorithm for determining which token id to return is:
+ *
+ * generate random_value between 0% and 100%
+ * cumulative_percentage = 0
+ * for (all token ids where the remaining balance to be claimed is > 0, and the token isn't a fallback option) {
+ *   if random_value is between cumulative_percentage and cumulative_percentage + token ids percentage {
+ *     allocate that token id
+ *   }
+ * }
+ * if (no token has been allocated) {
+ *   Allocate token from the first fallback option available.
+ * }
+ *
+ * This contract is designed to be upgradeable.
+ */
+contract FourteenNumbersClaimV2 is FourteenNumbersClaim {
+    error IncorrectPreviousVersion(uint256 _ver);
+
+    /// @notice Version 1 version number
+    uint256 internal constant _VERSION1 = 1;
+
+
+    /**
+     * @notice Function to be called when upgrading this contract.
+     * @dev Call this function as part of upgradeToAndCall().
+     *      This initial version of this function reverts. There is no situation
+     *      in which it makes sense to upgrade to the V0 storage layout.
+     *      Note that this function is permissionless. Future versions must
+     *      compare the code version and the storage version and upgrade
+     *      appropriately. As such, the code will revert if an attacker calls
+     *      this function attempting a malicious upgrade.
+     * @ param _data ABI encoded data to be used as part of the contract storage upgrade.
+     */
+    function upgradeStorage(bytes memory /* _data */) external override virtual {
+        if (version != _VERSION0) {
+            revert IncorrectPreviousVersion(version);
+        }
+        version = _VERSION1;
+    }
+
+    /**
+     * @notice Prepare for the claim call.
+     * @dev Register for generating a random value. 
+     * @param _salt A random value specified by the game app.
+     */
+    function prepareForClaim(uint256 _salt) external override whenNotPaused() {
+        // Do nothing
+    }
+
+    /**
+     * @notice Claim an NFT. 
+     */
+    function claim(uint256 /* ignored */) external override whenNotPaused() {
+        // Claims are only allowed from passport wallets.
+        if (!isPassport(msg.sender)) {
+            revert ClaimNonPassportAccount(msg.sender);
+        }
+
+        // Check that the player is eligible to claim.
+        (uint256 daysPlayed, uint256 claimedSoFar) = _checkAndUpdateClaimedDay();
+
+        // Calculate an on-chain random number.
+        uint256 randomValue = _generateRandom(block.number - 1);
+
+        // Select a NFT based on the random value.
+        (address nftContract, uint256 tokenId) = _determineRandomNft(randomValue);
+
+        // Transfer an NFT to the game player.
+        IERC1155 erc1155 = IERC1155(nftContract);
+        erc1155.safeTransferFrom(address(this), msg.sender, tokenId, 1, new bytes(0));
+
+        emit Claimed(msg.sender, nftContract, tokenId, daysPlayed, claimedSoFar);
+    }
+}
