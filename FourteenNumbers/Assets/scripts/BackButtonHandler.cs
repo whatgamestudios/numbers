a1afa026d82b15632e48df9b056276c463fe686c
Peter Robinson <drinkcoffee@eml.c>
2025-05-31 19:27:06 +1000

    First attempt at back button

--- FourteenNumbers/Assets/scripts/BackButtonHandler.cs
+++ FourteenNumbers/Assets/scripts/BackButtonHandler.cs
@@ -7,16 +7,35 @@ using TMPro;
 
 namespace FourteenNumbers {
 
-    public class BackButtonHandler : MonoBehaviour {
+    public class BackButtonHandler : MonoBehaviour
+    {
         public Button buttonBack;
 
-        public void OnButtonClick(string buttonText) {
-            if (buttonText == "Menu") {
+        public void OnButtonClick(string buttonText)
+        {
+            if (buttonText == "Menu")
+            {
                 SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
             }
-            else {
+            else
+            {
                 AuditLog.Log($"BackButton: Unknown button: {buttonText}");
             }
         }
+
+        public void Update()
+        {
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                if (Input.GetKey(KeyCode.Escape))
+                {
+                    // Insert Code Here (I.E. Load Scene, Etc)
+                    // OR Application.Quit();
+                    SceneManager.LoadScene("MenuScene", LoadSceneMode.Single);
+                    return;
+                }
+            }
+        }
+
     }
 }
